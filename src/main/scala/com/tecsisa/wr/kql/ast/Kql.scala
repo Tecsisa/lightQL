41b8f42fbccb6aa27354d7cf741e8a31cff242df
freinn <freinn@gmail.com>
2016-05-03 13:15:41 +0200

    Add the IN option and various bug fixes. fixes issue 1

--- src/main/scala/com/tecsisa/wr/kql/ast/Kql.scala
+++ src/main/scala/com/tecsisa/wr/kql/ast/Kql.scala
@@ -5,8 +5,10 @@ package ast
 trait Kql
 object Kql {
   case class DocumentType(name: String) extends Kql
+  case class IndexName(name: String) extends Kql
   case class Search(
     types: Seq[DocumentType],
+    indexes: Option[Seq[IndexName]] = None,
     limit: Option[Limit] = None
   ) extends Kql
   case class Limit(size: Int) extends Kql
