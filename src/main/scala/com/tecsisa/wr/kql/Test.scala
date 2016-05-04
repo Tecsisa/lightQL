16e9f07f5bc8777fe1f0b439ac1bf6e11d906596
freinn <freinn@gmail.com>
2016-05-04 15:18:40 +0200

    fixed #2, operators are now the right ones, fields aren't surrounded with double quotes and all works right

--- src/main/scala/com/tecsisa/wr/kql/Test.scala
+++ src/main/scala/com/tecsisa/wr/kql/Test.scala
@@ -3,10 +3,6 @@ package kql
 
 import com.tecsisa.wr.kql.ast.Kql._
 import fastparse.all._
-// search (t1, t2)  in (i1, i2, ...)
-// with limit x
-// with query ()
-// with filter ()
 
 object Test {
 
@@ -15,43 +11,75 @@ object Test {
       def apply(t: T) = f(t)
       override def toString() = name
     }
+
+    /***************** Helpers *****************/
+    // Note, quotedP does not capure, you have to call the bang '!' method in
+    // the quotedP argument (p) to make p capture input.
+    def quotedP[T](p: Parser[T]): Parser[T] = {
+      P(space ~ "\"" ~ p ~ "\"")
+    }
+    /************* End of helpers **************/
+
     val Whitespace = NamedFunction(" \r\n".contains(_: Char), "Whitespace")
     val Digits = NamedFunction('0' to '9' contains (_: Char), "Digits")
-    // ! es un lookahead negativo que se aplica sobre un parser
-    // añadimos el espacio como carácter que no puede aparecer
+    /*
+       ! Es un lookahead negativo que se aplica sobre un parser.
+       Añadimos el espacio como carácter que no puede aparecer como condición de
+       parada en el caso en que solo se especifica un tipo en la consulta.
+    */
     val DtChars = NamedFunction(!"\"\\_,() ".contains(_: Char), "DtChars")
+    // ForbiddenIndexChars is negated already
+    val ForbiddenIndexChars =
+      NamedFunction(!"./\\*?\"<>| ,()".contains(_: Char), "ForbiddenIndexChars")
 
     val space = P(CharsWhile(Whitespace).?)
     val digits = P(CharsWhile(Digits))
     val integral = P("0" | CharIn('1' to '9') ~ digits.?)
     val openParen = P(space ~ "(")
     val closeParen = P(space ~ ")")
-    def parenBlock[T](p: Parser[T]): Parser[Vector[T]] = openParen ~ p.rep(sep = ",").map(_.toVector) ~ closeParen
-    def block[T](p: Parser[T]): Parser[Vector[T]] = p.map(Vector(_)) | parenBlock(p)
+    def parenBlock[T](p: Parser[T]): Parser[Vector[T]] =
+      openParen ~ p.rep(sep = ",").map(_.toVector) ~ closeParen
+    def block[T](p: Parser[T]): Parser[Vector[T]] =
+      p.map(Vector(_)) | parenBlock(p)
 
     val dtChars = P(CharsWhile(DtChars))
     val dtName: Parser[DocumentType] = P(space ~ dtChars.!).map(DocumentType)
     val dtBlock = block(dtName)
-    // https://github.com/elastic/elasticsearch/issues/6736
 
-    val notAllowedIndexStartingChars = CharIn("./\\*?\"<>| ,()") // falta el .
-    val compoundingIndexChars = P(CharIn('a' to 'z', '0' to '9', "_-"))
-    val indexName: Parser[IndexName] = P(space ~ !notAllowedIndexStartingChars ~ compoundingIndexChars.rep.!).map(IndexName)
-    val indexesBlock = parenBlock(indexName)
+    // The forbidden index chars are based on this webpage:
+    // https://github.com/elastic/elasticsearch/issues/6736
+    val forbiddenIndexChars = P(CharsWhile(ForbiddenIndexChars))
+    val indexChars = P(CharIn('a' to 'z', '0' to '9', "_-"))
+    val indexName =
+      P(space ~ forbiddenIndexChars ~ indexChars.rep)
+    val indexesBlock = block(indexName.!.map(x => IndexName(x.trim)))
     val search = P(IgnoreCase("search"))
     val `with` = P(IgnoreCase("with"))
+    val query = P(IgnoreCase("query"))
     val limit = P(IgnoreCase("limit"))
     val in = P(IgnoreCase("in"))
+    val fieldP = P(indexChars.rep())
+    val operatorP = P("=" | "<=" | ">=" | "<>" | ">" | "<")
+
+    // Here we parse all characters other than double quotes
+    val valueP = P(integral | quotedP(CharPred(_ != '"').rep))
+    val queryClause = P(space ~ fieldP.! ~ space ~ operatorP.! ~ space ~ valueP.!).map(
+      x => Clause(Field(x._1), Operator(x._2), Value(x._3))
+    )
+
+    val queryBlock = block(queryClause)
 
     val searchSection = (search ~ dtBlock)
-    val inSection = (in ~ indexesBlock)
-    val limitSection = P(space ~ `with` ~ space ~ limit ~ space ~ integral.!).map(_.toInt).map(Limit)
+    val indexSection = (in ~ indexesBlock)
+    val querySection = (space ~ `with` ~ space ~ query ~ space ~ queryBlock)
+    val limitSection = P(space ~ `with` ~ space ~ limit ~ space
+      ~ integral.!).map(_.toInt).map(Limit)
 
-    val expr = P(space ~ searchSection ~ space ~ inSection.? ~ space ~ limitSection.? ~ space ~ End)
-      .map {
-        case (searchSection, inSection, limitSection) => Search(searchSection, inSection, limitSection)
-      }
-
+    val expr = P(space ~ searchSection ~ space ~ indexSection ~ limitSection.?
+      ~ querySection ~ space ~ End).map {
+      case (searchSection, indexSection, limitSection, querySection) =>
+        Search(searchSection, indexSection, limitSection, querySection)
+    }
   }
 
   // search (t1, t2)  in (i1, i2, ...)
@@ -63,18 +91,17 @@ object Test {
     import Parser._
 
     // Next three use cases must success
-    val target1 = "search (type1, type2, type3) with limit 10"
-    val target2 = "search type1 in (index3, index5) with limit 100"
-    val target3 = "search (type1, type2) in (index1, index2)"
+    val target1 = "search type1 in (index3,index5) with limit 100 with query (campo1 <> 24534, campo2 <> 1)"
+    val target2 = "search (type1, type2) in ( index1,            index2) with query (campo1 > \"sonciclossanos\")"
+    val target3 = "search (type1, type2) in ( index1,            index2) with query campo1 < \"sonciclossanos\"   "
+    // This must fail because the last parenthesis is missing
+    val target4 = "search (type1, type2) in (index1, index2) with query ( campo1 = \"sonciclossanos\""
+    // This must fail because the index section is missing
+    val target5 = "search (type1, type2, type3) with limit 10"
     // This must fail because has a dot in the beginning of a index name
-    val target4 = "search (type1, type2) in (.index1, index2)"
-    val r1 = expr.parse(target1)
-    val r2 = expr.parse(target2)
-    val r3 = expr.parse(target3)
-    val r4 = expr.parse(target4)
-    println(r1)
-    println(r2)
-    println(r3)
-    println(r4)
+    val target6 = "search (type1, type2) in (.index1, index2) with query (field = \"randomStr\")"
+
+    val testList = List(target1, target2, target3, target4, target5, target6)
+    testList.map(x => println(expr.parse(x)))
   }
 }
