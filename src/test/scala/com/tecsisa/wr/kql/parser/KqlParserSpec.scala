f8e24e56aaf3d5c8beab2f3ed0689272c230a7df
Juan José Vázquez <juanjo.vazquez.delgado@tecsisa.com>
2016-05-23 17:38:09 +0200

    Start the unit testing infrastructure (fixes #5)

---
+++ src/test/scala/com/tecsisa/wr/kql/parser/KqlParserSpec.scala
@@ -0,0 +1,21 @@
+package com.tecsisa.wr
+package kql
+package parser
+
+import com.tecsisa.wr.kql.ast.Kql.{DocumentType, IndexName, Search}
+import com.tecsisa.wr.kql.parser.KqlParser.expr
+
+class KqlParserSpec extends BaseTest {
+
+  "A KqlParser" should {
+    "parse a simple expression with only a single document type and index" in {
+      val dt     = "type1"
+      val index  = "index1"
+      val e      = s"search $dt in $index"
+      val parsed = expr.parse(e)
+      parsed shouldBe a[Success]
+      parsed.get.value shouldBe Search(Vector(DocumentType(dt)),
+                                       Vector(IndexName(index)))
+    }
+  }
+}
