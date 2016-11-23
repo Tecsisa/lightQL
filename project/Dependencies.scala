d8be6faadda087ec8eae34e8098ad064fc1292fa
Juan José Vázquez <juanjo.vazquez.delgado@tecsisa.com>
2016-11-23 14:17:02 +0100

    Elastic4s 2.3.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,7 +3,7 @@ import sbt._
 object Version {
   final val Scala               = "2.11.8"
   final val ScalaTest           = "3.0.1"
-  final val Elastic4s           = "2.4.0"
+  final val Elastic4s           = "2.3.1"
   final val Dsl                 = "0.1.0-SNAPSHOT"
   final val ElasticMaterializer = s"$Elastic4s-SNAPSHOT"
 }
