a48b121476fe5d54659fda7a66bffd3d23515ebb
Juan José Vázquez <juanjo.vazquez.delgado@tecsisa.com>
2016-11-24 18:03:47 +0100

    Prepare release 2.3.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,8 @@ object Version {
   final val Scala               = "2.11.8"
   final val ScalaTest           = "3.0.1"
   final val Elastic4s           = "2.3.1"
-  final val Dsl                 = "0.1.0-SNAPSHOT"
-  final val ElasticMaterializer = s"$Elastic4s-SNAPSHOT"
+  final val Dsl                 = "0.1.0"
+  final val ElasticMaterializer = "2.3.0"
 }
 
 object Library {
