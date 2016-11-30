0b24f7cf0caf95c9bf79380f2194bcb1d05407e7
Juan José Vázquez <juanjo.vazquez.delgado@tecsisa.com>
2016-11-30 21:25:06 +0100

    Prepare release 0.1.3 (dsl) and 5.0.2 (elastic)

--- project/Publish.scala
+++ project/Publish.scala
@@ -5,6 +5,7 @@ import com.typesafe.sbt.GitPlugin
   * For projects that are not to be published.
   */
 object NoPublish extends AutoPlugin {
+
   override def requires = plugins.JvmPlugin && GitPlugin
 
   override def projectSettings = Seq(
