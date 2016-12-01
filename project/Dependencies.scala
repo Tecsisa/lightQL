8d99a686a9f0400e5d54555d5421ae2b0fb715d1
Juan José Vázquez <juanjo.vazquez.delgado@tecsisa.com>
2016-12-01 17:08:55 +0100

    Preparation for the next development cycle

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Version {
   final val ScalaTest           = "3.0.1"
   final val Elastic4s           = "2.3.1"
   final val Dsl                 = "0.1.3"
-  final val ElasticMaterializer = "2.3.3"
+  final val ElasticMaterializer = "2.3.4-SNAPSHOT"
 }
 
 object Library {
