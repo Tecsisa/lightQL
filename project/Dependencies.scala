917f75d775688f977040dea34ab5cabb07ddc444
Juan José Vázquez <juanjo.vazquez.delgado@tecsisa.com>
2016-12-06 21:41:23 +0100

    Release 0.1.6 (dsl) and 5.0.5 (elastic)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,8 +6,8 @@ object Version {
   final val FastParse           = "0.4.2"
   final val Elastic4s           = "5.0.1"
   final val NscalaTime          = "2.14.0"
-  final val Dsl                 = "0.1.6-SNAPSHOT"
-  final val ElasticMaterializer = "5.0.5-SNAPSHOT"
+  final val Dsl                 = "0.1.6"
+  final val ElasticMaterializer = "5.0.5"
 }
 
 object Library {
