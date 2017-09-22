e928c452eb08a91b7880fe8ade721479504886dc
Francisco José Bermejo Herrera <francisco.bermejo.herrera@tecsisa.com>
2017-09-22 08:57:11 +0200

    Preparation of new development cycle

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,8 +6,8 @@ object Version {
   final val FastParse           = "0.4.4"
   final val Elastic4s           = "5.4.11"
   final val NscalaTime          = "2.16.0"
-  final val Dsl                 = "0.4.2"
-  final val ElasticMaterializer = "5.4.2"
+  final val Dsl                 = "0.4.3-SNAPSHOT"
+  final val ElasticMaterializer = "5.4.3-SNAPSHOT"
   final val Scalafmt            = "1.2.0"
 }
 
