e22554bc9baa6c88342af562903366d568c39f19
Francisco José Bermejo Herrera <francisco.bermejo.herrera@tecsisa.com>
2017-09-22 08:46:16 +0200

    Releases 0.4.2 (dsl) and 5.4.2 (elastic)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,8 +6,8 @@ object Version {
   final val FastParse           = "0.4.4"
   final val Elastic4s           = "5.4.11"
   final val NscalaTime          = "2.16.0"
-  final val Dsl                 = "0.4.2-SNAPSHOT"
-  final val ElasticMaterializer = "5.4.2-SNAPSHOT"
+  final val Dsl                 = "0.4.2"
+  final val ElasticMaterializer = "5.4.2"
   final val Scalafmt            = "1.2.0"
 }
 
