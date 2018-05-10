053084fb16925bc9a69eb56f9d0b7cbcaec1b234
Francisco José Bermejo Herrera <francisco.bermejo.herrera@tecsisa.com>
2018-05-10 08:48:51 +0200

    Update to elastic4s 6.2.8, Scala 2.12.6 and SBT 1.1.5.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,10 +1,10 @@
 import sbt._
 
 object Version {
-  final val ScalaVersions       = Seq("2.12.4", "2.11.12")
+  final val ScalaVersions       = Seq("2.12.6", "2.11.12")
   final val ScalaTest           = "3.0.5"
   final val FastParse           = "1.0.0"
-  final val Elastic4s           = "6.2.3"
+  final val Elastic4s           = "6.2.8"
   final val NscalaTime          = "2.18.0"
   final val Log4j               = "2.9.1"
   final val Scalafmt            = "1.4.0"
