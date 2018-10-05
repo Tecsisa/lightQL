093dbb921585735417d5397e3a6c2fa4e132bcd9
Francisco José Bermejo Herrera <francisco.bermejo.herrera@tecsisa.com>
2018-10-05 13:09:40 +0200

    Update to Scala 2.12.7 and SBT 1.2.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Version {
-  final val ScalaVersions       = Seq("2.12.6", "2.11.12")
+  final val ScalaVersions       = Seq("2.12.7", "2.11.12")
   final val ScalaTest           = "3.0.5"
   final val FastParse           = "1.0.0"
   final val Elastic4s           = "6.2.9"
