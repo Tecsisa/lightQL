062e693a8d125d3ae340881419c7b905d4af7e41
Álvaro Gómez Giménez <alvaro.gomez.gimenez@tecsisa.com>
2020-04-16 13:12:09 +0200

    Release 0.11.3 (dsl) and 6.7.3 (Elastic)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -7,8 +7,8 @@ object Version {
   final val Elastic4s           = "6.7.2"
   final val NscalaTime          = "2.20.0"
   final val Log4j               = "2.9.1"
-  final val Dsl                 = "0.11.3-SNAPSHOT"
-  final val ElasticMaterializer = "6.7.3-SNAPSHOT"
+  final val Dsl                 = "0.11.3"
+  final val ElasticMaterializer = "6.7.3"
 }
 
 object Library {
